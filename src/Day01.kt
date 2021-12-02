707d88408e8e0c9b1a0e22c4f7034f56ac67c9e7
SekthDroid <dev@sekthdroid.com>
2021-12-02 01:20:12 +0100

    Added day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,32 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.sumOfBigger()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }
+            .windowed(3)
+            .map { it.sum() }
+            .sumOfBigger()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(testInput))
+    check(part1(testInput) == 7)
+
+    println(part2(testInput))
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+private fun List<Int>.sumOfBigger(): Int = foldIndexed(0) { index, acc, i ->
+    if (getOrElse(index + 1) { 0 } > i) {
+        acc + 1
+    } else {
+        acc
+    }
 }
